53ce2e2de6649c798dec758726b75aa31a754b9b
Ramon Ferreira <ramondiegoaf@hotmail.com>
2022-11-04 13:08:46 -0300

    create routes mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,10 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto';
-
+import { Routes } from "./src/routes";
 import { THEME } from './src/styles/theme'
 import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/SignIn';
-import { Pools } from './src/screens/Pools';
 import { AuthContextProvider } from "./src/contexts/AuthContext";
+
 
 
 export default function App() {
@@ -20,7 +19,7 @@ export default function App() {
           translucent
         />
         
-        { fontsLoaded ? <SignIn /> : <Loading /> }
+        { fontsLoaded ? <Routes /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
